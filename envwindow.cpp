96bdbc884ea231cc218fb89a9bf876e6dc9f322f
浓咖啡 <1075518049@qq.com>
2017-06-01 21:25:39 +0800

    加入模拟数据

--- envwindow.cpp
+++ envwindow.cpp
@@ -6,9 +6,29 @@ EnvWindow::EnvWindow(QWidget *parent) :
     ui(new Ui::EnvWindow)
 {
     ui->setupUi(this);
+    ui->widget->setVisible(false);
+    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
+    timer = new QTimer;
+    QObject::connect(timer, SIGNAL(timeout()), this,  SLOT(timeoutSlot()));
+    timer->start(1000);
 }
 
 EnvWindow::~EnvWindow()
 {
     delete ui;
 }
+
+void EnvWindow::timeoutSlot()
+{
+    temp = qrand()%20;
+    hum = qrand()%50;
+    ill = qrand()%20;
+//    bet = qrand()%3;
+    adc = qrand()%100;
+
+    ui->tableWidget->setItem(0,0,new QTableWidgetItem(QString::number(temp, 10)));
+    ui->tableWidget->setItem(1,0,new QTableWidgetItem(QString::number(hum, 10)));
+    ui->tableWidget->setItem(2,0,new QTableWidgetItem(QString::number(ill, 10)));
+    ui->tableWidget->setItem(4,0,new QTableWidgetItem(QString::number(adc, 10)));
+}
+
--- envwindow.h
+++ envwindow.h
@@ -2,6 +2,8 @@
 #define ENVWINDOW_H
 
 #include <QtWidgets>
+#include <QtGlobal>
+#include <QTimer>
 
 namespace Ui {
 class EnvWindow;
@@ -14,9 +16,20 @@ class EnvWindow : public QWidget
 public:
     explicit EnvWindow(QWidget *parent = 0);
     ~EnvWindow();
+    void anolog();
+
+private slots:
+    void timeoutSlot();
 
 private:
     Ui::EnvWindow *ui;
+    int temp;
+    int hum;
+    int ill;
+    int bet;
+    int adc;
+
+    QTimer *timer;
 };
 
 #endif // ENVWINDOW_H
